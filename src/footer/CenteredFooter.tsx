7bea5d13cb4fbb12d6fd232564c79fe8a8ca90e6
cheney <cheney@sonder.io>
2023-08-09 00:20:14 +1000

    update

--- src/footer/CenteredFooter.tsx
+++ src/footer/CenteredFooter.tsx
@@ -6,18 +6,11 @@ import { FooterIconList } from './FooterIconList';
 type ICenteredFooterProps = {
   logo: ReactNode;
   iconList: ReactNode;
-  children: ReactNode;
 };
 
 const CenteredFooter = (props: ICenteredFooterProps) => (
   <div className="text-center">
     {props.logo}
-
-    <nav>
-      <ul className="navbar mt-5 flex flex-row justify-center text-xl font-medium text-gray-800">
-        {props.children}
-      </ul>
-    </nav>
 
     <div className="mt-8 flex justify-center">
       <FooterIconList>{props.iconList}</FooterIconList>
